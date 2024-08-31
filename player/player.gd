dc28f1398054ab24e11289f47c14ea53ee1d385f
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2024-08-31 16:04:37 +0100

    fix attached player script

--- player/player.gd
+++ player/player.gd
@@ -1,19 +1,16 @@
-extends CharacterBody2D
+# class_name gives a name to this type of CharacterBody2D
+# So in other scripts if I want to make a new "Player"
+# I can use stuff like Player.new()
+# And that would make a new McSpeeeeeeeed
+class_name Player extends CharacterBody2D
 
 # @export lets you modify it in the left "inspector" tab
 # means you can change the speed without modifying the script
-@export var max_speed: int = 1000
+@export var max_speed: int = 300
 # can use ": int" to type hint that it's an int
 # also useful to catch errors
-@export var acceleration: int = 5
-@export var friction: int = 5
-
-# can use "move_towards" instead: https://docs.godotengine.org/en/stable/classes/class_vector2.html#class-vector2-method-move-toward
-# does incrementation by a fixed amount - aka acceleration
-#func calculate_acceleration():
-		#var speedRatio = currentSpeed / MAXSPEED #Gets a fraction signifying how close it is to top speed
-		#var acceleration = sin(3.141 * (1 - speedRatio))
-		#return acceleration
+@export var acceleration: int = 20
+@export var friction: int = 10
 
 func _physics_process(_delta):
 	look_at(get_global_mouse_position())
@@ -26,3 +23,9 @@ func _physics_process(_delta):
 		velocity = velocity.move_toward(transform.x * forward_or_backward * max_speed, acceleration)
 	
 	move_and_slide()
+
+func boost() -> void:
+	var _old_speed = max_speed
+	var _old_acceleration = acceleration
+	max_speed = max_speed * 2
+	acceleration = acceleration * 2
