c00b096d32f36bc01eff22d8f4c35fdc4161ab4d
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2024-08-31 16:14:09 +0100

    added particles emitting only when moving

--- player/player.gd
+++ player/player.gd
@@ -6,11 +6,20 @@ class_name Player extends CharacterBody2D
 
 # @export lets you modify it in the left "inspector" tab
 # means you can change the speed without modifying the script
+@export_category("Movement") # export categories let you
+# put export variables in categories on the right pane
 @export var max_speed: int = 300
 # can use ": int" to type hint that it's an int
 # also useful to catch errors
 @export var acceleration: int = 20
 @export var friction: int = 10
+
+@export_category("Animations")
+# we can assign these in the right pane,
+# linked it to "TrailParticles" so we can use it within the script
+# other way you can do this is with $TrailParticles
+# but that isn't as clean
+@export var exhaust_particles: CPUParticles2D
 
 func _physics_process(_delta):
 	look_at(get_global_mouse_position())
@@ -19,8 +28,10 @@ func _physics_process(_delta):
 	# if we are not pressing anything, move our velocity towards (0,0) (x,y)
 	if forward_or_backward == 0:
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
+		exhaust_particles.emitting = false
 	else: # if we are pressing, accelerate towards our transform * input * max speed
 		velocity = velocity.move_toward(transform.x * forward_or_backward * max_speed, acceleration)
+		exhaust_particles.emitting = true
 	
 	move_and_slide()
 
